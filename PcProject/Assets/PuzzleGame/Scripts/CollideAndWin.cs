84562f77f592ef55a04d547f915e7b70625b344c
AntonioSantos8 <afsantos082008@gmail.com>
2025-03-12 14:06:42 -0300

    comit

--- PcProject/Assets/PuzzleGame/Scripts/CollideAndWin.cs
+++ PcProject/Assets/PuzzleGame/Scripts/CollideAndWin.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CollideAndWin : MonoBehaviour
 {
-    public bool winTheGame;
+    public static bool winTheGame;
 
     // Update is called once per frame
     void Update()
